8dae06492fea59c7f04d165eb76274f3898f31c0
Karamba <102661780+20kambarba06@users.noreply.github.com>
2022-04-10 21:43:04 +0300

    price_7000
    price_7000

---
+++ script.js
@@ -0,0 +1,99 @@
+////////////1/////////
+//let user = prompt('What is your name?').toLowerCase()
+//let userCar = prompt('What is you car?')
+//let carPrice = +prompt('How much?')
+//let maxPrice = 7000
+//let cheap = 0
+
+//if(user === user){
+    
+//if (userCar === userCar ){
+//    if(carPrice === carPrice){
+//       
+//        if(carPrice < maxPrice){
+//            cheap++,
+//            cheap = `Mr/Mrs:${user}` + `; ` +
+//                    `Car is:${userCar}` + `; ` +
+//                    `Car price:${carPrice}`,
+//            alert(`
+//            Mr/Mrs:${user}
+//            Car is:${userCar}
+//            Car price:${carPrice}
+//            `)
+//        }else{
+  //          alert(`Mr/Mrs you car too expensive, I can not buy it`)
+//        }
+//    }
+//}
+//}
+
+
+////////////2/////////
+
+
+//let arr = [1, 2, false, `hello`, 24, `world`, undefined, null, `error`, 24]
+//let num = 0
+//let bool = 0
+//let obj = 0
+//let str = 0
+//let und = 0 
+
+//arr.filter(item => {
+//    if(typeof(item) === 'number') {
+//        num++
+//    } else if(typeof(item) === 'object') {
+//        obj++
+//    } else if(typeof(item) === 'boolean') {
+//        bool++
+//    } else if(typeof(item) === 'string') {
+//        str++
+//    } else if(typeof(item) === 'undefined' ) {
+//       und++
+//   }
+//})
+
+//console.log(
+//    num,bool,obj,str,und
+//);
+
+
+////////////3/////////
+
+
+//let arr = [1, 2, false, `hello`, 24, `world`, undefined, null, `error`, 24]
+//let num = 0
+//arr.filter(item => {
+//    if(typeof(item) === `number`){
+//        num++}
+//}) 
+//if(num > 5){
+//    console.log(`Good`);
+//}else{
+//    console.log(`Sorry`);
+//}
+
+
+////////////4/////////
+
+
+//let arr = [`Aleksey`, `Kartoshka`, `Margarita`, `Morgenshtern`, `Monica`]
+
+//let index = +prompt(`Who delete: Aleksey; Kartoshka; Margarita; Morgenshtern; Monica`)
+//arr.splice((index), 1)
+//console.log(arr);
+
+
+////////////5/////////
+
+
+//let arr = [1, 2, false, `hello`, 24, `world`, undefined, null, `error`, 24]
+//let num = 0
+//arr.filter(item => {
+//    if(typeof(item) === `number`){
+//        num++}
+//}) 
+//if(num > 5){
+//    console.log(`Good`);
+//}else{
+//    console.log(`Sorry`);
+//}
